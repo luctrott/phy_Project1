841dfce70927d7728be47ddfaf6ed6e215ea8107
luctrott <120565362+luctrott@users.noreply.github.com>
2023-11-13 10:10:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 MOTOR_NAME = "motorix"
 # Settings for connecting the client to the broker
 # Change this to your pi after setting up the broker
-BROKER = "192.168.130.12"
+BROKER = "10.1.48.108"
 # topic for controlling the motor
 TOPIC_MOTOR = f'motors/{MOTOR_NAME}/control'
 # pin config for the motor
