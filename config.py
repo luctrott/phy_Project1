0b8b07219cb6145ffe1b14fd645cb8b250831649
luctrott <120565362+luctrott@users.noreply.github.com>
2023-11-13 10:14:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BROKER = "10.1.48.108"
 # topic for controlling the motor
 TOPIC_MOTOR = f'motors/{MOTOR_NAME}/control'
 # pin config for the motor
-MOTOR_IX_PIN = 17
+MOTOR_IX_PIN = 4
 MOTOR_IX_ID = 1
 
 DB_NAME = "db.db"
